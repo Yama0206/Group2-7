a339634b741e44108d7fb98ee39111b7eb1a5a2a
Yama0206 <hiyamakouta2@gmail.com>
2024-04-10 14:02:23 +0900

    main.cpp up
    Main.cpp にScene.hをインクルード

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 //ウィンドウを表示するプログラム（ひな形）
 
 #include "DxLib.h"	//DXライブラリのインクルード
+#include "Scene/Scene.h"
 
 // define
 #define	SCREEN_SIZE_X	640	// X方向の画面サイズを指定
@@ -25,6 +26,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	
 	//-----------------------------------------
 	//一番最初に１回だけやる処理をここに書く
+
 
 	//-----------------------------------------
 
